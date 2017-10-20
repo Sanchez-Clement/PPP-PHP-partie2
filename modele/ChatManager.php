d5769bb4d748580235b514cb28590a2c5753697b
Clement Sanchez <sanchez.clmnt@gmail.com>
2017-10-20 14:27:02 +0200

    add the function addChat in the class ChatManager

---
+++ modele/ChatManager.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *
+ */
+class ChatManager
+{
+private $bdd;
+  function __construct($bdd)
+  {
+  $this->setBdd($bdd);
+  }
+
+  public function setBdd($bdd)
+  {
+  $this->bdd=$bdd;
+  }
+  public function addChat(Chat $chat)
+  {
+    $reponse = $this->bdd->prepare('INSERT INTO Chat(nom,sexe,age,color) VALUES(:nom,:sexe,:age,:color)');
+    $reponse->execute([
+'nom' => $chat->getNom(),
+'sexe' => $chat->getSexe(),
+'age' => $chat->getAge(),
+'color' => $chat->getColor()
+
+    ]);
+  }
+}
+ ?>
